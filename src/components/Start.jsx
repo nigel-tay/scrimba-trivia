75cd0be343093178d888e2c42e2cc3c2001e9da1
nigel-tay <ninenigel@gmail.com>
2022-12-01 15:44:54 +0800

    Add background images with styles for Start component

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import top from '../assets/images/topright.png';
+import bot from '../assets/images/botleft.png'
 
 export default function Start() {
   return (
     <div className="start-container">
+        <img className="top" src={top} />
+        <img className="bot" src={bot} />
         <h1 className='karla'>Quizzical</h1>
         <h2 className="inter">A fun trivia game for all ages!</h2>
         <button className="inter">Start Quiz</button>
