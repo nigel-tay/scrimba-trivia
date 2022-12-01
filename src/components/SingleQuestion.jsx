1425ab3c9f6015f6c84beea77265f358a43330b4
nigel-tay <ninenigel@gmail.com>
2022-12-01 17:26:35 +0800

    Finish SingleQuestion component skeleton

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 export default function SingleQuestion({questions}) {
-    
+
     return (
         <div className="singlequestion-container">
             {
-                questions.map(question => (
+                questions.map(({question, correct_answer, incorrect_answers}) => (
                     <div key={uuidv4()}>
-                        <h1>{question.question}</h1>
-
+                        <h1>{question}</h1>
+                        <button>{correct_answer}</button>
+                        {incorrect_answers.map(answer => (
+                            <button>{answer}</button>
+                        ))}
                     </div>
                 ))
             }
