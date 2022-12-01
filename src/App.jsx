c364775edc5968feb46f78a42c3902cd189af287
nigel-tay <ninenigel@gmail.com>
2022-12-01 15:32:20 +0800

    Add Start and Questions components, and content with styles for Start component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import Start from "./components/Start";
+import Questions from "./components/Questions";
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Start />
+      {/* <Questions /> */}
     </div>
   )
 }
---
+++ src/components/Start.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Start() {
+  return (
+    <div className="start-container">
+        <h1 className='karla'>Quizzical</h1>
+        <h2 className="inter">A fun trivia game for all ages!</h2>
+        <button className="inter">Start Quiz</button>
+    </div>
+  )
+}
